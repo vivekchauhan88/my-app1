13a0b7969dd838784e115815ffe9d2debe9c1a6e
Vivek Chauhan <vivekchauhan67@yahoo.com>
2021-02-14 15:45:19 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,48 +1,142 @@
-//import React, { Component } from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import React from "react";
 
-import Home from './components/Home';
-import About from './components/About';
-import Contact from './components/Contact';
-import Error from './components/Error';
-import Navigation from './components/Navigation';
-import {Nav} from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import Firebase from "firebase";
+import config from "./config";
 
-function App() {
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    Firebase.initializeApp(config);
+
+    this.state = {
+      developers: []
+    };
+  }
+
+  componentDidMount() {
+    this.getUserData();
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState !== this.state) {
+      this.writeUserData();
+    }
+  }
+
+  writeUserData = () => {
+    Firebase.database()
+      .ref("/")
+      .set(this.state);
+    console.log("DATA SAVED");
+  };
+
+  getUserData = () => {
+    let ref = Firebase.database().ref("/");
+    ref.on("value", snapshot => {
+      const state = snapshot.val();
+      this.setState(state);
+    });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    let name = this.refs.name.value;
+    let uid = this.refs.uid.value;
+
+    if (uid && name) {
+      const { developers } = this.state;
+      const devIndex = developers.findIndex(data => {
+        return data.uid === uid;
+      });
+      developers[devIndex].name = name;
+      this.setState({ developers });
+    } else if (name) {
+      const uid = new Date().getTime().toString();
+      const { developers } = this.state;
+      developers.push({ uid, name });
+      this.setState({ developers });
+    }
+
+    this.refs.name.value = "";
+    this.refs.uid.value = "";
+  };
+
+  removeData = developer => {
+    const { developers } = this.state;
+    const newState = developers.filter(data => {
+      return data.uid !== developer.uid;
+    });
+    this.setState({ developers: newState });
+  };
+
+  updateData = developer => {
+    this.refs.uid.value = developer.uid;
+    this.refs.name.value = developer.name;
+  };
+
+  render() {
+    const { developers } = this.state;
     return (
-       <BrowserRouter>
-          {/*<Navigation />*/}
-          <Nav className="navr" activeKey="/">
-    <Nav.Item>
-      <Nav.Link href="/">HOME</Nav.Link>
-    </Nav.Item>
-    <Nav.Item>
-      <Nav.Link href="/about/">ABOUT</Nav.Link>
-    </Nav.Item>
-    <Nav.Item>
-      <Nav.Link href="/opensource/">OPEN SOURCE</Nav.Link>
-    </Nav.Item>
-    <Nav.Item>
-      <Nav.Link href="/solutions/">SOLUTIONS</Nav.Link>
-    </Nav.Item>
-    <Nav.Item>
-      <Nav.Link href="/insights/">BLOG</Nav.Link>
-    </Nav.Item>
-    <Nav.Item>
-      <Nav.Link href="/contact/">CONTACT</Nav.Link>
-    </Nav.Item>
-  </Nav>
+      <React.Fragment>
+        <div className="container">
+          <div className="row">
+            <div className="col-xl-12">
+              <h1>To-do App</h1>
+            </div>
+          </div>
 
-            <Switch>
-             <Route path="/" component={Home} exact/>
-             <Route path="/about" component={About}/>
-             <Route path="/contact" component={Contact}/>
-            <Route component={Error}/>
-           </Switch>
-           </BrowserRouter>
-
+          <div className="row">
+            <div className="col-xl-12">
+              <form onSubmit={this.handleSubmit}>
+                <div className="form-row">
+                  <input type="hidden" ref="uid" />
+                  <div className="form-group col-md-6">
+                    <input
+                      type="text"
+                      ref="name"
+                      className="form-control"
+                      placeholder="Add a task"
+                    />
+                  </div>
+                </div>
+                <button type="submit" className="btn btn-primary">
+                  Create
+                </button>
+              </form>
+            </div>
+          </div>
+          <br />
+          <div className="row">
+            <div className="col-xl-12">
+              {developers.map(developer => (
+                <div
+                  key={developer.uid}
+                  className="list-group-item"
+                  
+                >
+                  
+                    <h5>{developer.name}</h5>
+                    <button
+                      onClick={() => this.removeData(developer)}
+                      className="btn btn-link"
+                    >
+                      Delete
+                    </button>
+                    <button
+                      onClick={() => this.updateData(developer)}
+                      className="btn btn-link"
+                    >
+                      Edit
+                    </button>
+                  
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </React.Fragment>
     );
+  }
 }
 
 export default App;
---
+++ src/config.js
@@ -0,0 +1,10 @@
+const config = {
+    apiKey: "AIzaSyANcmo_G1utvF5wqci6PdqDw-ZVL4pHUGM",
+    authDomain: "my-app1-b11fa.firebaseapp.com",
+    projectId: "my-app1-b11fa",
+    storageBucket: "my-app1-b11fa.appspot.com",
+    messagingSenderId: "80760935706",
+    appId: "1:80760935706:web:31e5a0271313cd80df88a3"
+  };
+  
+  export default config;
